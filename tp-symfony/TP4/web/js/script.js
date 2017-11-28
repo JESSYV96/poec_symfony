5faaed53e988135e8589d17772f2f8e149862829
ellorex <lea.rerolle@gmail.com>
2017-11-28 01:40:18 +0100

    more navbar, still not there

---
+++ tp-symfony/TP4/web/js/script.js
@@ -0,0 +1,10 @@
+// $(document).ready(function() {
+//   $(window).scroll(function() {
+//     if ($(window).scrollTop() >= 70) {
+//       $('nav').addClass('navbar_fixed');
+//     } else {
+//       $('nav').removeClass('navbar_fixed');
+//     }
+//   });
+//
+// });
